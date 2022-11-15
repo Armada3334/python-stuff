2c6d88675e42b7b841aa9e57417451333363e5b8
Armada <73616858+Armada3334@users.noreply.github.com>
2022-11-15 01:25:50 -0500

    start

---
+++ needed.ps1
@@ -0,0 +1,7 @@
+try {
+    python3 -m pip install --upgrade pip
+    pip3 install argparse pytermgui
+}
+catch {
+    exit(1)
+}
---
+++ needed.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+python3 -m pip install --upgrade pip
+pip3 install argparse pytermgui
