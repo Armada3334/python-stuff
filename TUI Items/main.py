59a365c774d428cad6ea07beeaec938c16622e62
Jeremy MacRoberts <jeremy.macroberts@gmail.com>
2022-11-15 14:55:16 -0500

    More testing

--- TUI Items/main.py
+++ TUI Items/main.py
@@ -1,18 +1,21 @@
 import npyscreen
 
 # TUI that gets IP address, netmask, and gateway from the user
-class myFunctionSelector(npyscreen.Form):
+class connectTUI(npyscreen.Form):
         def create(self):
-                ip = self.add(npyscreen.TitleText, name = "IP Address: ")
+                ip = self.add(npyscreen.TitleText, name = "Management IP Address: ")
                 netmask = self.add(npyscreen.TitleText, name = "Netmask: ")
+                username = self.add(npyscreen.TitleText, name = "Administrator Username: ")
+                password = self.add(npyscreen.TitleText, name = "Password: ")
         def afterEditing(self):
-                self.parentApp.setNextForm(None)
+                self.parentApp.setNextForm(podMAN)
 
 
-class MyApplication(npyscreen.NPSAppManaged):
+class Application(npyscreen.NPSAppManaged):
     def onStart(self):
-        self.addForm('MAIN', myFunctionSelector, name='Select Function')
-
+        self.addForm('MAIN', connectTUI, name='Pod Manager')
+        
+        
 if __name__ == '__main__':
-    TestApp = MyApplication().run()
+    TestApp = Application().run()
     print ("Program Complete")
